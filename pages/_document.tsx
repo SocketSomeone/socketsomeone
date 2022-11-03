608f4ac798265cbec031a578e6d95ccb8c320b49
SocketSomeone <socket.someone@gmail.com>
2022-11-03 10:23:09 +0300

    feat: add gtag

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,4 +1,5 @@
 import {Html, Head, Main, NextScript} from 'next/document'
+import Script from "next/script";
 
 export default function Document() {
     return (
@@ -8,7 +9,21 @@ export default function Document() {
             <body className="dark:bg-gray-900">
             <Main/>
             <NextScript/>
+            <Script
+                src="https://www.googletagmanager.com/gtag/js?id=G-FGL4QES69Q"
+                strategy="afterInteractive"
+            />
+            <Script id="google-analytics" strategy="afterInteractive">
+                {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-FGL4QES69Q');
+        `}
+            </Script>
             </body>
+
         </Html>
     )
 }
