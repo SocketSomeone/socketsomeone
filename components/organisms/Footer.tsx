cf4bf96f38d610ca14e6dd3e2ecb650b0a82ccf9
SocketSomeone <socket.someone@gmail.com>
2024-08-12 18:10:57 +0300

    feat: add legal info

--- components/organisms/Footer.tsx
+++ components/organisms/Footer.tsx
@@ -1,5 +1,6 @@
 export default function Footer() {
     return <footer className="relative px-4 py-0 justify-center align-center text-center">
-        <p className="text-center text-xs font-medium mb-4">SocketSomeone • {new Date().getFullYear()}</p>
+        <p className="text-center text-xs font-medium mb-2">SocketSomeone • {new Date().getFullYear()}</p>
+        <p className="text-center text-xs font-medium mb-4">ИП: Филиппов А.Н. • ИНН: 391105816033 • ОГРНИП: 324390000002554</p>
     </footer>
 }
