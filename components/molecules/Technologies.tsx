d7d8d2427957505b03dd6780347206dfa8424d4d
SocketSomeone <socket.someone@gmail.com>
2024-09-23 14:06:16 +0300

    refactor: remove problem

--- components/molecules/Technologies.tsx
+++ components/molecules/Technologies.tsx
@@ -29,24 +29,6 @@ export const renderCustomIcon = (icon: SimpleIcon, theme: string) => {
 };
 
 export default function Technologies() {
-  const technologies = [
-    // React
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1200px-React-icon.svg.png',
-    // Tailwind
-    'https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/tailwind-css-icon.png',
-    // PostgreSQL
-    'https://upload.wikimedia.org/wikipedia/commons/thumb/2/29/Postgresql_elephant.svg/1200px-Postgresql_elephant.svg.png',
-    // MongoDB
-    'https://cdn.iconscout.com/icon/free/png-256/free-mongodb-3629020-3030245.png',
-    // GRPC
-    'https://grpc.io/img/logos/grpc-icon-color.png',
-    // Redis
-    'https://cdn.iconscout.com/icon/free/png-256/free-redis-4-1175103.png',
-    // RabbitMQ
-    'https://www.vectorlogo.zone/logos/rabbitmq/rabbitmq-icon.svg',
-    // Nginx
-    'https://d4.alternativeto.net/rzVs59edzRVa55KMDeoptD3jkE1Ulv-DycWvE_c4nkw/rs:fill:280:280:0/g:ce:0:0/YWJzOi8vZGlzdC9pY29ucy9uZ2lueF8xMDIwMzUucG5n.png',
-  ];
   const [data, setData] = useState<IconData | null>(null);
   const { theme } = useTheme();
   const iconSlugs = [
@@ -66,8 +48,7 @@ export default function Technologies() {
     'grpc',
     'redis',
     'rabbitmq',
-    'nginx',
-    '1password'
+    'nginx'
   ];
 
   useEffect(() => {
