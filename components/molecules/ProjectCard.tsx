f32e230dd0e041ea412b9e998e317f6a755d4eb6
SocketSomeone <socket.someone@gmail.com>
2025-03-23 04:28:46 +0300

    feat: add embla for allowing drag projects marquee

--- components/molecules/ProjectCard.tsx
+++ components/molecules/ProjectCard.tsx
@@ -37,7 +37,8 @@ export default function ProjectCard({
 	return (
 		<a
 			href={url}
-			className="flex flex-row pointer-events-auto py-2 px-4 mx-2 my-4 w-[22rem] sm:w-[26rem] md:w-[24rem] lg:w-[26rem] xl:w-[30rem] rounded-xl bg-white dark:bg-gray-800 shadow-xl shadow-black/5 ring-1 ring-slate-700/10 hover:bg-slate-50 dark:ring-gray-800 dark:hover:ring-gray-700 cursor-pointer">
+			className="flex flex-row h-[180px] pointer-events-auto py-2 px-4 mx-2 my-4 w-[22rem] sm:w-[26rem] md:w-[24rem] lg:w-[26rem] xl:w-[30rem] rounded-xl
+			bg-white dark:bg-gray-800 shadow-xl shadow-black/5 ring-1 ring-slate-700/10 hover:bg-slate-50 dark:ring-gray-800 dark:hover:ring-gray-700 cursor-pointer">
 			<img
 				src={thumbnail}
 				alt={title}
@@ -57,7 +58,7 @@ export default function ProjectCard({
 				</div>
 
 
-				<p className="text-gray-500 flex-1 text-sm font-normal dark:text-gray-300">{description ?? 'No description'}</p>
+				<p className="text-gray-500 flex-grow text-sm font-normal dark:text-gray-300">{description ?? 'No description'}</p>
 
 				<Metrics style={'mt-2 justify-start dark:text-blue-500'} forks={forks} stars={stars}
 						 issues={issues ?? 0} language={language ?? 'Unknown'}/>
--- components/organisms/MarqueeProjectsHorizontal.tsx
+++ components/organisms/MarqueeProjectsHorizontal.tsx
@@ -1,5 +1,7 @@
 import ProjectCard from '@/components/molecules/ProjectCard';
 import Marquee from '@/components/magicui/marquee';
+import { Carousel, CarouselContent, CarouselItem } from '../magicui/carousel';
+import AutoScroll from 'embla-carousel-auto-scroll';
 
 export interface MarqueeProjectsHorizontalProps {
 	projects: any;
@@ -28,31 +30,50 @@ export default function MarqueeProjectsHorizontal({projects, rows}: MarqueeProje
 
 	return (
 		<div
-			className="flex flex-col flex-wrap items-center justify-center rounded-lg border bg-background bg-gray-100 border-gray-300 bg-opacity-80 backdrop-blur-sm dark:bg-gray-800 dark:border-gray-800 dark:bg-opacity-40">
+			className="flex flex-col flex-wrap rounded-lg border bg-background bg-gray-100 border-gray-300 bg-opacity-80 backdrop-blur-sm dark:bg-gray-800 dark:border-gray-800 dark:bg-opacity-40">
 			{
 				projectsRows.map((row: any[], index: number) => (
-					<Marquee key={index} pauseOnHover reverse={index % 2 === 0} className={'[--duration:60s]'}>
-						{row.map((project: any, i: number) => (
-							<ProjectCard key={i} url={project.html_url || project.homepage}
-										 thumbnail={project.owner.login !== 'SocketSomeone' ? project.owner.avatar_url : 'icons/github_gradient.svg'}
-										 title={project.name} description={project.description}
-										 metrics={{
-											 forks: project.forks,
-											 stars: project.stargazers_count,
-											 issues: project.open_issues,
-											 language: project.language
-										 }}
-										 newest={new Date(project.created_at) > threeMonthsAgo}
-										 hotest={mostPopularProjects.includes(project.id)}/>
-						))}
-					</Marquee>
+					<Carousel key={index}
+							  opts={{
+								  loop: true,
+								  dragFree: true
+							  }}
+						plugins={[AutoScroll({
+							speed: 2,
+							direction:  index % 2 === 0 ? 'backward' : 'forward',
+							stopOnInteraction: false,
+							startDelay: 1
+						})]}
+							  className={'w-[100vw]'}
+					>
+						<CarouselContent className={''}>
+							{row.map((project: any, i: number) => (
+
+								<CarouselItem className={'basis-auto'} key={i}>
+									<ProjectCard url={project.html_url || project.homepage}
+												 thumbnail={project.owner.login !== 'SocketSomeone' ? project.owner.avatar_url : 'icons/github_gradient.svg'}
+												 title={project.name} description={project.description}
+												 metrics={{
+													 forks: project.forks,
+													 stars: project.stargazers_count,
+													 issues: project.open_issues,
+													 language: project.language
+												 }}
+												 newest={new Date(project.created_at) > threeMonthsAgo}
+												 hotest={mostPopularProjects.includes(project.id)}/>
+								</CarouselItem>
+
+							))}
+						</CarouselContent>
+
+					</Carousel>
 				))
 			}
 
 			<div
-				className="pointer-events-none absolute inset-y-0 left-0 w-1/2 sm:w-1/4 md:w-1/2  bg-gradient-to-r from-white dark:from-background"></div>
+				className="pointer-events-none absolute inset-y-0 left-0 w-0 sm:w-1/4 md:w-1/3  bg-gradient-to-r from-white dark:from-background"></div>
 			<div
-				className="pointer-events-none absolute inset-y-0 right-0 w-1/2 sm:w-1/4 md:w-1/2 bg-gradient-to-l from-white dark:from-background"></div>
+				className="pointer-events-none absolute inset-y-0 right-0 w-0 sm:w-1/4 md:w-1/3 bg-gradient-to-l from-white dark:from-background"></div>
 		</div>
 	);
 }
