bc1195fd1fe187d32b7072facb9fc9560d3a3965
DaiBoh <ilyaindastris@mail.ru>
2025-03-26 16:40:58 +0300

    fix: corrected comments after review

--- components/organisms/Education.tsx
+++ components/organisms/Education.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import {TimelineEvent, TimelineHeader} from '../molecules/Timeline';
+import {TimelineEvent} from '../molecules/Timeline';
 import {BookOpenIcon} from '@heroicons/react/24/solid';
-import {ChevronRightIcon} from 'lucide-react';
+
 
 interface EducationEntry {
     name: string;
@@ -32,19 +32,11 @@ export default function Education() {
     return (
         <div className="md:container text-muted-foreground self-center pt-10 w-full relative">
             {education.map((school, index) => (
-                <TimelineEvent key={`timeline-education-${index}`} active={false} last={index === education.length - 1}>
+                <TimelineEvent key={`timeline-education-${index}`} active={school.name === 'Bonch-burevich'} last={index === education.length - 1}>
                     <div className="flex flex-col">
                         <div className="flex flex-row items-center text-blue-500">
                             {school.icon}
                             <div className="flex flex-row items-start justify-between w-full">
-                                <TimelineHeader>
-                                    <div className="flex-col gap-1 ml-4 inline-flex font-semibold leading-none text-xs sm:text-sm">
-                                        <div className="flex items-center text-foreground">
-                                            {school.name}
-                                            <ChevronRightIcon className="w-3 md:w-4 md:ml-1" />
-                                        </div>
-                                    </div>
-                                </TimelineHeader>
                                 <small className="text-xs sm:text-sm tabular-nums text-muted-foreground text-right">
                                     {school.startDate} - {school.endDate}
                                 </small>
