e3d714c91dfbe53109e6e76e06280b3a8d986e4d
SocketSomeone <socket.someone@gmail.com>
2025-05-16 16:17:27 +0300

    feat: add download icon for cv

--- components/molecules/CVDownloader.tsx
+++ components/molecules/CVDownloader.tsx
@@ -1,20 +1,23 @@
 import { useCallback } from 'react';
 import { cn } from '@/utils';
+import { DownloadIcon } from 'lucide-react';
 
 export default function CVDownloader(props: {}) {
 	const openPrintMode = useCallback(() => window.print(), []);
 
 	return (
 		<button
-			className="print:hidden absolute top-5 right-15 z-50 flex items-center justify-center px-4 h-6 mt-1 rounded-full bg-white dark:bg-gray-800 ring-2 ring-slate-600/10 dark:ring-slate-300/10 hover:bg-slate-50 select-none cursor-pointer"
+			className="print:hidden absolute top-5 right-15 z-50 space-x-1 flex items-center justify-center px-4 h-6 mt-1 rounded-full bg-white dark:bg-gray-800 ring-2 ring-slate-600/10 dark:ring-slate-300/10 hover:bg-slate-50 select-none cursor-pointer"
 			onClick={openPrintMode}
 			aria-label="Download CV"
 		>
-				<span
-					className={cn(
-						`truncate text-sm inline animate-gradient text-foreground`,
-					)}
-				>
+			<DownloadIcon width={14}/>
+
+			<span
+				className={cn(
+					`truncate text-sm inline animate-gradient text-foreground`,
+				)}
+			>
 				  Download CV
 				</span>
 		</button>
