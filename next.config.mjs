7259191763221a47bf822f0dd1101997890f74e8
SocketSomeone <socket.someone@gmail.com>
2025-06-21 03:13:45 +0300

    style: lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,33 +1,33 @@
-import createPWAPlugin from "next-pwa";
+import createPWAPlugin from 'next-pwa';
 
 const withPWA = createPWAPlugin({
-    dest: 'public',
-    register: true,
-    skipWaiting: true,
-    disable: process.env.NODE_ENV === 'development'
+	dest: 'public',
+	register: true,
+	skipWaiting: true,
+	disable: process.env.NODE_ENV === 'development'
 })
 
 /** @type {import('next').NextConfig} */
 const nextConfig = withPWA({
-    reactStrictMode: true,
-    output: 'export',
-    images: {
-        unoptimized: true,
-        remotePatterns: [
-            {
-                protocol: "https",
-                hostname: "cdn.discordapp.com"
-            },
-            {
-                protocol: "https",
-                hostname: "cdn.discord.com"
-            },
-            {
-                protocol: "https",
-                hostname: "avatars.githubusercontent.com"
-            }
-        ]
-    }
+	reactStrictMode: true,
+	output: 'export',
+	images: {
+		unoptimized: true,
+		remotePatterns: [
+			{
+				protocol: 'https',
+				hostname: 'cdn.discordapp.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'cdn.discord.com'
+			},
+			{
+				protocol: 'https',
+				hostname: 'avatars.githubusercontent.com'
+			}
+		]
+	}
 })
 
 export default nextConfig;
