fd0307816083c90f02c47561afd4883578b05e1a
SocketSomeone <socket.someone@gmail.com>
2025-07-05 12:03:30 +0300

    feat: add rotate animation for logo

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -36,12 +36,13 @@ export default function Header() {
 	return (
 		<>
 			<SupportModal isOpen={supportModalOpen} onClose={() => setSupportModalOpen(false)}/>
+
 			<header
 				className="fixed top-0 left-0 px-4 xl-wide:top-4 md:left-1/2 md:-translate-x-1/2 w-full xl-wide:container z-50 print:hidden bg-background/60 dark:bg-gray-900/60 backdrop-blur-xs border-b-1 xl-wide:border-1 rounded-none xl-wide:rounded-xl border-gray-200 dark:border-gray-700 flex justify-between items-center h-[64px]">
 				<div className="flex items-center space-x-4">
 					<Link href="/"
 						  className="text-xl flex flex-row items-center space-x-2 text-gray-800 dark:text-gray-200 hover:text-gray-900 dark:hover:text-gray-100 transition-colors duration-300">
-						<Logo width={32} height={32}/>
+						<Logo className='transition-transform sm:transform hover:-rotate-6 flex-shrink-0' width={32} height={32}/>
 
 						<div className="flex flex-col">
 							<small className="text-sm">SocketSomeone</small>
