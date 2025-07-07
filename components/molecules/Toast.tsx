405b4371a6dd417be2bdf1199342083a4214b409
SocketSomeone <socket.someone@gmail.com>
2025-07-07 21:34:32 +0300

    fix: disable toast layout

--- components/molecules/Toast.tsx
+++ components/molecules/Toast.tsx
@@ -75,8 +75,6 @@ export default function Toast() {
 			{!closed && activity && (
 				<motion.div
 					key="toast"
-					layout
-					layoutId="toast"
 					initial={{ opacity: 0, y: 50 }}
 					animate={{ opacity: 1, y: 0 }}
 					exit={{ opacity: 0, y: 50 }}
