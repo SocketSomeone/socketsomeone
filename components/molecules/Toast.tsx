49f029e39f44831436e08cedb3a5eda24eb287fc
SocketSomeone <socket.someone@gmail.com>
2025-07-07 21:24:11 +0300

    feat: add animations for toast

--- components/molecules/Toast.tsx
+++ components/molecules/Toast.tsx
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useState } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
 import { cn, dateToDuration } from '@/utils';
 import Image from 'next/image';
 import { ClockIcon, XMarkIcon } from '@heroicons/react/24/solid';
@@ -69,106 +70,116 @@ export default function Toast() {
 	}, [status]);
 
 
-	if (closed || !activity) {
-		return;
-	}
-
 	return (
-		<div className="fixed bottom-2 right-2 z-99 print:hidden">
-			<div id="toast-message-cta"
-				 className="w-full max-w-md p-4 text-gray-500 bg-white rounded-xl shadow-2xl shadow-black/5 ring-1 ring-slate-700/10 dark:bg-gray-800 dark:text-gray-400"
-				 role="alert">
-				<div className="flex">
-					<div className="flex flex-col">
-						<div className="mb-2 text-sm font-semibold text-gray-900 dark:text-blue-500">
-							My current activity
-						</div>
-
-						<div className="flex items-center gap-3">
-							<div className={cn('relative flex h-max items-center', {
-								hidden: !largeImage && !smallImage,
-							})}>
-								{largeImage && (
-									<div className="relative inline-block group">
-										<Image
-											className="h-full w-auto max-h-[80px] rounded-md object-contain"
-											onError={({ currentTarget }) => currentTarget.style.display = 'none'}
-											width={4096}
-											height={4096}
-											quality={100}
-											src={largeImage}
-											alt="Large Image"
-										/>
-
-										<div
-											role="tooltip"
-											className={cn(
-												'absolute bottom-full left-1/2 -translate-x-1/2 mb-2 z-50 px-3 py-2 text-sm font-medium rounded-lg shadow-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300 pointer-events-none whitespace-nowrap',
-												'bg-gray-900 text-white dark:bg-gray-200 dark:text-gray-900',
-												{ 'hidden': !activity.assets?.large_text }
-											)}
-										>
-											{activity.assets?.large_text || 'Unknown Application'}
-										</div>
-									</div>
-								)}
-
-								{smallImage && (
-									<div className="absolute group -bottom-1 -right-1">
-										<Image
-											className="max-w-[28px] w-auto h-auto border-2 border-white bg-white dark:border-gray-800 dark:bg-gray-800 rounded-full"
-											onError={({ currentTarget }) => currentTarget.style.display = 'none'}
-											width={4096}
-											height={4096}
-											quality={100}
-											src={smallImage}
-											alt="Small Icon"
-										/>
-
-										<div
-											role="tooltip"
-											className={cn(
-												'absolute bottom-full left-1/2 -translate-x-1/2 mb-0.5 z-50 px-3 py-2 text-sm font-medium rounded-lg shadow-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300 pointer-events-none whitespace-nowrap',
-												'bg-gray-900 text-white dark:bg-gray-200 dark:text-gray-900',
-												{ 'hidden': !activity.assets?.small_text }
-											)}
-										>
-											{activity.assets?.small_text}
-										</div>
-									</div>
-								)}
-							</div>
-
-							<div className="text-sm font-normal whitespace-pre-line min-w-[120px] max-w-[220px]">
-								<div className="text-sm font-semibold text-gray-900 dark:text-white space-y-0.5">
-									{activity.name}
+		<AnimatePresence>
+			{!closed && activity && (
+				<motion.div
+					key="toast"
+					layout
+					layoutId="toast"
+					initial={{ opacity: 0, y: 50 }}
+					animate={{ opacity: 1, y: 0 }}
+					exit={{ opacity: 0, y: 50 }}
+					transition={{ duration: 0.5 }}
+					className="fixed bottom-2 right-2 z-99 print:hidden"
+				>
+					<div
+						className="w-full max-w-md p-4 text-gray-500 bg-white rounded-xl shadow-2xl shadow-black/5 ring-1 ring-slate-700/10 dark:bg-gray-800 dark:text-gray-400"
+						role="alert"
+					>
+						<div className="flex">
+							<div className="flex flex-col">
+								<div className="mb-2 text-sm font-semibold text-gray-900 dark:text-blue-500">
+									My current activity
 								</div>
-								{activity.details && (
-									<div className="text-sm font-normal break-words">{activity.details}</div>
-								)}
-								{activity.state && (
-									<div className="text-sm font-normal truncate">{activity.state}</div>
-								)}
-								{(elapsed || left) && (
-									<div className="flex items-center text-blue-600 dark:text-blue-500">
-										<ClockIcon width={16} height={16} className="inline-block mr-1"/>
-										<div className="text-sm font-normal ">
-											{left ? `${left} left` : `${elapsed} elapsed`}
-										</div>
+
+								<div className="flex items-center gap-3">
+									<div className={cn('relative flex h-max items-center', {
+										hidden: !largeImage && !smallImage,
+									})}>
+										{largeImage && (
+											<div className="relative inline-block group">
+												<Image
+													className="h-full w-auto max-h-[80px] rounded-md object-contain"
+													onError={({ currentTarget }) => currentTarget.style.display = 'none'}
+													width={4096}
+													height={4096}
+													quality={100}
+													src={largeImage}
+													alt="Large Image"
+												/>
+
+												<div
+													role="tooltip"
+													className={cn(
+														'absolute bottom-full left-1/2 -translate-x-1/2 mb-2 z-50 px-3 py-2 text-sm font-medium rounded-lg shadow-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300 pointer-events-none whitespace-nowrap',
+														'bg-gray-900 text-white dark:bg-gray-200 dark:text-gray-900',
+														{ 'hidden': !activity.assets?.large_text }
+													)}
+												>
+													{activity.assets?.large_text || 'Unknown Application'}
+												</div>
+											</div>
+										)}
+
+										{smallImage && (
+											<div className="absolute group -bottom-1 -right-1">
+												<Image
+													className="max-w-[28px] w-auto h-auto border-2 border-white bg-white dark:border-gray-800 dark:bg-gray-800 rounded-full"
+													onError={({ currentTarget }) => currentTarget.style.display = 'none'}
+													width={4096}
+													height={4096}
+													quality={100}
+													src={smallImage}
+													alt="Small Icon"
+												/>
+
+												<div
+													role="tooltip"
+													className={cn(
+														'absolute bottom-full left-1/2 -translate-x-1/2 mb-0.5 z-50 px-3 py-2 text-sm font-medium rounded-lg shadow-lg opacity-0 group-hover:opacity-100 group-hover:visible transition-opacity duration-300 pointer-events-none whitespace-nowrap',
+														'bg-gray-900 text-white dark:bg-gray-200 dark:text-gray-900',
+														{ 'hidden': !activity.assets?.small_text }
+													)}
+												>
+													{activity.assets?.small_text}
+												</div>
+											</div>
+										)}
 									</div>
-								)}
+
+									<div className="text-sm font-normal whitespace-pre-line min-w-[120px] max-w-[220px]">
+										<div className="text-sm font-semibold text-gray-900 dark:text-white space-y-0.5">
+											{activity.name}
+										</div>
+										{activity.details && (
+											<div className="text-sm font-normal break-words">{activity.details}</div>
+										)}
+										{activity.state && (
+											<div className="text-sm font-normal truncate">{activity.state}</div>
+										)}
+										{(elapsed || left) && (
+											<div className="flex items-center text-blue-600 dark:text-blue-500">
+												<ClockIcon width={16} height={16} className="inline-block mr-1"/>
+												<div className="text-sm font-normal ">
+													{left ? `${left} left` : `${elapsed} elapsed`}
+												</div>
+											</div>
+										)}
+									</div>
+								</div>
+
+								<ActivityButton activity={activity}/>
 							</div>
+
+							<CloseButton
+								onClick={() => setClosed(true)}
+							/>
 						</div>
-
-						<ActivityButton activity={activity}/>
 					</div>
-
-					<CloseButton
-						onClick={() => setClosed(true)}
-					/>
-				</div>
-			</div>
-		</div>
+				</motion.div>
+			)}
+		</AnimatePresence>
 	);
 }
 
