0046d884383e54a8c6e5137c11795f0925ab0dd8
SocketSomeone <socket.someone@gmail.com>
2025-07-17 18:53:02 +0300

    feat: add contacts page

--- components/organisms/Projects/Projects.tsx
+++ components/organisms/Projects/Projects.tsx
@@ -36,7 +36,7 @@ export default function Projects({ type, background }: ProjectsProps): JSX.Eleme
 
 			<div
 				className={cn('flex flex-row justify-center w-full rounded-lg', {
-					'bg-gray-100/80 border-gray-300 backdrop-blur-xs dark:bg-gray-800/40 dark:border-gray-800 border': background,
+					'bg-gray-100/80 border-gray-300 backdrop-blur-xs dark:bg-gray-800/40 dark:border-gray-800 border overflow-hidden': background,
 					'bg-transparent dark:bg-transparent': !background
 				})}>
 				{
