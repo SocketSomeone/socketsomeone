9a6792e6dd299ce046eb4cbc8b65a3e7de1ac7e4
SocketSomeone <socket.someone@gmail.com>
2025-09-22 15:35:36 +0300

    fix: add browser language detection for redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,19 @@
 'use client';
 
 import { redirect } from '@/i18n/navigation';
+import { routing } from '@/i18n/routing';
+import { hasLocale } from 'next-intl';
 
 export default function Page() {
+	const browserLocale = navigator.language.split('-')[0];
+	let localeToRedirect = 'en';
+
+	if (hasLocale(routing.locales, browserLocale)) {
+		localeToRedirect = browserLocale;
+	}
+
 	return redirect({
 		href: '/',
-		locale: 'en',
+		locale: localeToRedirect,
 	});
 }
