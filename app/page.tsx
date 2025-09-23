3b998d5030d3014ac88be8a9544408a851696c90
SocketSomeone <socket.someone@gmail.com>
2025-09-23 13:08:04 +0300

    fix: use use effect navigator as web api

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,24 @@
 'use client';
 
+import { useEffect } from 'react';
 import { redirect } from '@/i18n/navigation';
 import { routing } from '@/i18n/routing';
 import { hasLocale } from 'next-intl';
 
 export default function Page() {
-	const browserLocale = navigator.language.split('-')[0];
-	let localeToRedirect = 'en';
+	useEffect(() => {
+		const browserLocale = navigator.language.split('-')[0];
+		let localeToRedirect = 'en';
 
-	if (hasLocale(routing.locales, browserLocale)) {
-		localeToRedirect = browserLocale;
-	}
+		if (hasLocale(routing.locales, browserLocale)) {
+			localeToRedirect = browserLocale;
+		}
 
-	return redirect({
-		href: '/',
-		locale: localeToRedirect,
-	});
+		redirect({
+			href: '/',
+			locale: localeToRedirect,
+		});
+	}, []);
+
+	return null; // пока редирект не сработает
 }
