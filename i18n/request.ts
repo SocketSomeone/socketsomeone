11c4bd6090cdbfca84f012dd24adfb39c8565886
SocketSomeone <socket.someone@gmail.com>
2025-09-30 20:39:28 +0300

    feat: add crowdin integrations
    # Conflicts:
    #	yarn.lock

--- i18n/request.ts
+++ i18n/request.ts
@@ -5,6 +5,6 @@ export default getRequestConfig(async ({ locale }) => {
 
 	return {
 		locale: currentLocale,
-		messages: (await import(`../messages/${currentLocale}.json`)).default,
+		messages: (await import(`../messages/${currentLocale}/main.json`)).default,
 	};
 });
