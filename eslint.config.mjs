17c5bb24284dc36aed303f8b88c4c5c446e2ed37
SocketSomeone <socket.someone@gmail.com>
2025-10-29 19:04:32 +0300

    fix: eslint problems

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,31 +1,16 @@
-import { FlatCompat }    from '@eslint/eslintrc';
-import { dirname }       from 'node:path';
-import { fileURLToPath } from 'node:url';
+import { FlatCompat } from '@eslint/eslintrc'
 
 const compat = new FlatCompat({
-	baseDirectory: dirname(fileURLToPath(import.meta.url)),
-});
+	baseDirectory: import.meta.dirname,
+})
 
 const eslintConfig = [
-	...compat.config({
-		extends: ['next/core-web-vitals', 'next/typescript'],
-	}),
+	...compat.extends('next/core-web-vitals', 'next/typescript'),
 	{
 		rules: {
 			'@typescript-eslint/no-explicit-any': 'off',
-			'object-curly-spacing': ['error', 'always']
-		},
+		}
 	},
-	{
-		ignores: [
-			'node_modules/**',
-			'.next/**',
-			'out/**',
-			'build/**',
-			'next-env.d.ts',
-			'public/**',
-		],
-	},
-];
+]
 
 export default eslintConfig
